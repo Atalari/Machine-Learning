022e8980dc484247773af4623a199d03fb9becd9
Atalari <152647840+Atalari@users.noreply.github.com>
2023-12-03 15:41:10 -0600

    Update index.js

--- Deployment/Churn_Modelling/index.js
+++ Deployment/Churn_Modelling/index.js
@@ -1,42 +1,46 @@
 async function runExample() {
+    // Initialize the array for inputs
+    var x = new Float32Array(14);
 
-    var x = new Float32Array( 1, 14 )
+    // Collect input values
+    x[0] = parseFloat(document.getElementById('box1').value);
+    x[1] = parseFloat(document.getElementById('box2').value);
+    x[2] = parseFloat(document.getElementById('box3').value);
+    x[3] = parseFloat(document.getElementById('box4').value);
+    x[4] = parseFloat(document.getElementById('box5').value);
+    x[5] = parseFloat(document.getElementById('box6').value);
+    x[6] = parseFloat(document.getElementById('box7').value);
+    x[7] = parseFloat(document.getElementById('box8').value);
+    x[8] = parseFloat(document.getElementById('box9').value);
+    x[9] = parseFloat(document.getElementById('box10').value);
+    x[10] = parseFloat(document.getElementById('box11').value);
+    x[11] = parseFloat(document.getElementById('box12').value);
+    x[12] = parseFloat(document.getElementById('box13').value);
+    x[13] = parseFloat(document.getElementById('box14').value);
 
-    var x = [];
-
-     x[1] = document.getElementById('box1').value;
-     x[2] = document.getElementById('box2').value;
-     x[3] = document.getElementById('box3').value;
-     x[4] = document.getElementById('box4').value;
-     x[5] = document.getElementById('box5').value;
-     x[6] = document.getElementById('box6').value;
-     x[7] = document.getElementById('box7').value;
-     x[8] = document.getElementById('box8').value;
-     x[9] = document.getElementById('box9').value;
-     x[10] = document.getElementById('box10').value;
-     x[11] = document.getElementById('box11').value;
-     x[12] = document.getElementById('box12’).value;
-     x[13] = document.getElementById('box12’).value;
-     x[14] = document.getElementById('box14’).value;
-
+    // Create a tensor
     let tensorX = new onnx.Tensor(x, 'float32', [1, 14]);
 
+    // Create a session and load the model
     let session = new onnx.InferenceSession();
+    try {
+        await session.loadModel("./DLnet_ChurnModel.onnx");
+        let outputMap = await session.run([tensorX]);
+        let outputData = outputMap.get('output1');
 
-    await session.loadModel("DLnet_ChurnModel.onnx");
-    let outputMap = await session.run([tensorX]);
-    let outputData = outputMap.get('output1');
-
-   let predictions = document.getElementById('predictions');
-
-  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-   <table>
-     <tr>
-       <td>  Rating of Churn_Modelling  </td>
-       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
-     </tr>
-  </table>`;
-    
-
-
+        // Display the predictions
+        let predictions = document.getElementById('predictions');
+        predictions.innerHTML = `
+            <hr> Got an output tensor with values: <br/>
+            <table>
+                <tr>
+                    <td>Rating of Churn_Modelling</td>
+                    <td id="td0">${outputData.data[0].toFixed(2)}</td>
+                </tr>
+            </table>`;
+    } catch (error) {
+        console.error(error);
+        // Handle errors here
+    }
 }
+
