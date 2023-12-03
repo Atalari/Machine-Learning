96649f97c07f63ad5b0105abf584004486f08c55
Atalari <152647840+Atalari@users.noreply.github.com>
2023-12-03 15:13:45 -0600

    Update index.js

--- Deployment/Churn_Modelling/index.js
+++ Deployment/Churn_Modelling/index.js
@@ -1,25 +1,25 @@
 async function runExample() {
 
-    var x = new Float32Array( 1, 11 )
+    var x = new Float32Array( 1, 14 )
 
     var x = [];
 
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
-     x[5] = document.getElementById('box6').value;
-     x[6] = document.getElementById('box7').value;
-     x[7] = document.getElementById('box8').value;
-     x[8] = document.getElementById('box9').value;
-     x[9] = document.getElementById('box10').value;
-     x[10] = document.getElementById('box11').value;
-     x[11] = document.getElementById('box12’).value;
+     x[1] = document.getElementById('box1').value;
+     x[2] = document.getElementById('box2').value;
+     x[3] = document.getElementById('box3').value;
+     x[4] = document.getElementById('box4').value;
+     x[5] = document.getElementById('box5').value;
+     x[6] = document.getElementById('box6').value;
+     x[7] = document.getElementById('box7').value;
+     x[8] = document.getElementById('box8').value;
+     x[9] = document.getElementById('box9').value;
+     x[10] = document.getElementById('box10').value;
+     x[11] = document.getElementById('box11').value;
      x[12] = document.getElementById('box12’).value;
-     x[13] = document.getElementById('box14’).value;
+     x[13] = document.getElementById('box12’).value;
+     x[14] = document.getElementById('box14’).value;
 
-    let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
+    let tensorX = new onnx.Tensor(x, 'float32', [1, 14]);
 
     let session = new onnx.InferenceSession();
 
@@ -32,7 +32,7 @@ async function runExample() {
   predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
    <table>
      <tr>
-       <td>  Rating of Wine Quality  </td>
+       <td>  Rating of Churn_Modelling  </td>
        <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
      </tr>
   </table>`;
